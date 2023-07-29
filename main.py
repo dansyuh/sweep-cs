b38e8adc6c4aff555414ff293a490e53f5c51384
dansyuh <dan@minekings,org>
2023-07-29 13:20:52 +0100

    clean up

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from random import randint
-from datetime import date
 
 grid = []
-positions = {}
+
+print("Simplified MineSweeper - CS Class Project")
 
 for it in range(0, 3):
     grid.append(["⬛️"] * 3)
@@ -21,17 +21,10 @@ def random_y():
     return randint(0, len(grid[0]) - 1)
 
 
-bomb1 = random_x()
-bomb2 = random_y()
+randomX = random_x()
+randomY = random_y()
 
 score = 0
-
-print("""
-💣 MINESWEEPER 💣
-
-⛏️= IS GOOD
-💣 = IS BAD
-""")
 
 for turn in range(4):
     x = int(input("What row you would like to select?\n"))
@@ -40,33 +33,24 @@ for turn in range(4):
     if x > 3 or y > 3:
         continue
 
-    if grid[x -1][y - 1] == "⛏️":
-        print("You have already chosen this slot!")
+    if grid[x - 1][y - 1] == "⛏️":
+        print("You have already selected this slot, please input a new entry.")
         continue
 
-    if bomb1 == x and bomb2 == y:
+    if randomX == x and randomY == y:
         grid[x - 1][y - 1] = "💣"
         print_grid()
         print("You loose! You hit the bomb! 💣")
         break
 
     if score == 3:
-        print("""
-        👑 YOU WIN! Congratulations you won the game!
-        """)
-
-        for it in positions:
-            print(" ".join(it))
+        print("You win! You did not hit the bomb! 👑")
         break
 
     if score > 1:
         print(f"You need {3 - score} more misses to win!")
 
     grid[x - 1][y - 1] = "⛏️"
-    positions[str(score)] = {
-        "move": f"{x}, {y}",
-    }
-
     score = score + 1
+
     print_grid()
-    print(positions)
